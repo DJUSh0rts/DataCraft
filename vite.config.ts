102637a1e5715c0501c4a11bdb1c4aa3fa44e6f1
DJUSh0rts <146638970+DJUSh0rts@users.noreply.github.com>
2025-09-24 14:17:13 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/DataCraft/",  // must match repo name exactly (case-sensitive)
+  base: "/DataCraft/", // must match repo name exactly
   plugins: [react()],
 });
