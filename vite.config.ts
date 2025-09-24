f1d64e247c91e7947083d8fef484872cb44e6010
DJUSh0rts <146638970+DJUSh0rts@users.noreply.github.com>
2025-09-24 14:11:14 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/DataCraft/",        // your repo name
+  base: "/DataCraft/",  // repo name
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
-  optimizeDeps: {
-    include: ["monaco-editor", "@monaco-editor/react"],
-  },
+  build: { outDir: "dist", sourcemap: true },
 });
